0159cd4f46bacad1254fe9a6d06f215a684239c8
jaco07 <115810219+jaco07@users.noreply.github.com>
2023-07-10 18:22:23 -0300

    Add files via upload

---
+++ prova/index.js
@@ -0,0 +1,31 @@
+04
+// const fs = require('fs')
+// fs.appendFile('novo.txt', 'Messi.txt', function (err, data) {
+//     if (err) {
+
+//     }
+//     console.log('Renomeando')
+// })
+
+// 05
+// const os = require('os')
+// console.log(os.cpus())
+03
+// const url = require('url')
+// const urlNova = ('https://org/api/url.html#url_new_url_input_base')
+// const newUrl = new url.URL(urlNova)
+// const objetos = {
+//     protocol:newUrl.protocol,
+//     slashes:true,
+//     auth:null,
+//     host:newUrl.host,
+//     port:null,
+//     hosthname:newUrl.hostname,
+//     hash:newUrl.protocol,
+//     search:null,
+//     query:newUrl.query,
+//     pathname:newUrl.pathname,
+//     path:newUrl.path,
+//     href:newUrl.href,
+// }
+// console.log(objetos)
